aff30bd992faadbf927bd181e0f6bc852f0d9235
MrSuricate <kevindu032270@gmail.com>
2024-03-07 09:50:11 +0100

    [RED] APlusB 2,3

--- Calculator.Test/CalculatorTest.cs
+++ Calculator.Test/CalculatorTest.cs
@@ -6,6 +6,7 @@ namespace Calculator.Test
 		[InlineData("1,1", 2)]
 		[InlineData("1,2", 3)]
 		[InlineData("2,2", 4)]
+		[InlineData("2,3", 5)]
 		public void APlusB(string input, int attendu)
 		{
 			int result = StringCalculator.Add(input);
